0bc6bdec79f2fa4c3924d739b266dfe3cecf7936
Brayden Tomlinson <humortronic@gmail.com>
2024-11-01 21:37:22 +1030

    Added changes to code

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,8 +6,48 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 21:05:51 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/01 21:06:38 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/01 21:37:05 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <iostream>
+
+const int Fixed::_fractionalBits = 8;
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) : _value(other._value)
+{
+    std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        this->_value = other._value;
+    }
+    return *this;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits() const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_value = raw;
+}
