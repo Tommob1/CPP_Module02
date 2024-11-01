29e60ae7d44d00dff0910cb2f9343061c200eb96
Brayden Tomlinson <humortronic@gmail.com>
2024-11-01 21:29:18 +1030

    Added changes to code

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,26 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 21:06:04 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/01 21:06:06 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/01 21:29:01 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+    private:
+        int _value;
+        static const int _fractionalBits;
+    public:
+        Fixed();
+        Fixed(const Fixed &other);
+        Fixed &operator=(const Fixed &other);
+        ~Fixed();
+
+        int getRawBits() const;
+        void setRawBits(int const raw);
+};
+
+#endif
