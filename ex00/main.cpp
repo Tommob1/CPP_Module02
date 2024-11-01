f0965c36817d2d63aa1309241789e7b73635f206
Brayden Tomlinson <humortronic@gmail.com>
2024-11-01 21:43:04 +1030

    Added changes to code

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,23 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 21:06:12 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/01 21:06:50 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/01 21:39:49 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <iostream>
+
+int main()
+{
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+
+    return (0);
+}
