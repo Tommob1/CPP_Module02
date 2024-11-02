34ad23e60084ce4413681bb47b9e57122394bca3
Brayden Tomlinson <humortronic@gmail.com>
2024-11-02 17:28:02 +1030

    Added changes to code:

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,13 +6,30 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 21:48:21 by btomlins          #+#    #+#             */
-/*   Updated: 2024/11/01 21:49:22 by btomlins         ###   ########.fr       */
+/*   Updated: 2024/11/02 17:27:42 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Fixed.hpp"
 
-int main()
-{
-    return (0);
+int main() {
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+
+    a = Fixed(1234.4321f);
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    return 0;
 }
